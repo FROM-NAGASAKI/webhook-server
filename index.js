903f83ef971298e5cb667fdbcad79d1e2f012dd5
Murakami <murakami@from-nagasaki.jp>
2026-06-02 13:06:31 +0900

    FB_SAテスト

--- index.js
+++ index.js
@@ -2,32 +2,10 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-// デバッグ：環境変数の確認
-console.log('=== 環境変数デバッグ ===');
-console.log('FIREBASE_SERVICE_ACCOUNT exists:', !!process.env.FIREBASE_SERVICE_ACCOUNT);
-console.log('FIREBASE_SERVICE_ACCOUNT length:', process.env.FIREBASE_SERVICE_ACCOUNT ? process.env.FIREBASE_SERVICE_ACCOUNT.length : 0);
-console.log('PAGE_ACCESS_TOKEN exists:', !!process.env.PAGE_ACCESS_TOKEN);
-console.log('全環境変数キー:', Object.keys(process.env).join(', '));
-console.log('=== デバッグ終了 ===');
-
-const VERIFY_TOKEN = 'union_support_verify_2024';
-
-app.get('/webhook', (req, res) => {
-  const mode = req.query['hub.mode'];
-  const token = req.query['hub.verify_token'];
-  const challenge = req.query['hub.challenge'];
-  if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-    res.status(200).send(challenge);
-  } else {
-    res.sendStatus(403);
-  }
-});
-
-app.post('/webhook', (req, res) => {
-  res.status(200).send('EVENT_RECEIVED');
-});
+console.log('FB_SA exists:', !!process.env.FB_SA);
+console.log('FB_SA value:', process.env.FB_SA);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log('サーバー起動中 ポート:', PORT);
+  console.log('起動 ポート:', PORT);
 });
